3d7b5b2c19768a7360a806fc3579f6bc1e6671e8
Anton Melnychuk <melnychukantoxa@gmail.com>
2019-11-02 19:17:09 +0200

    row hight nav bar title

--- PlaceBook/MainViewController.swift
+++ PlaceBook/MainViewController.swift
@@ -23,10 +23,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
      }
      
      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-        cell?.textLabel?.text = restaraunts[indexPath.row]
-        cell?.imageView?.image = UIImage(named: restaraunts[indexPath.row])
-        return cell!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = restaraunts[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaraunts[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
+        return cell
      }
+    
+    //MARK: Table view delegate
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
 }
 
