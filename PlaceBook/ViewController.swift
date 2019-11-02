a281d1dee4056bc8aaf38b1dbaa2c23d1a1bfea0
Anton Melnychuk <melnychukantoxa@gmail.com>
2019-11-02 13:22:08 +0200

    Initial Commit

---
+++ PlaceBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlaceBook
+//
+//  Created by MacBook on 11/2/19.
+//  Copyright © 2019 Anton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
